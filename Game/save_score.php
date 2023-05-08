a3654faa65a3d4cc472060a63b486a16c0fdf430
Matteo Tacconi <matteo@Matteos-iMac.local>
2023-05-08 16:12:12 +0200

    Minor bug fixes

--- Game/save_score.php
+++ Game/save_score.php
@@ -11,9 +11,11 @@ $conn = mysqli_connect('sql112.epizy.com', 'epiz_33814679', '6J6Q52XHDpNT1', 'ep
 $sql = "SELECT * FROM Players WHERE username='$username'";
 $result = mysqli_query($conn, $sql);
 
+// Get the current timestamp
+$timestamp = date('Y-m-d H:i:s');
+
 // If the username is not present, insert the data into the database
 if(mysqli_num_rows($result) == 0) {
-    $timestamp = date('Y-m-d H:i:s');
     $sql = "INSERT INTO Players (mode, username, score, timestamp) VALUES ('$mode', '$username', '$score', '$timestamp')";
     mysqli_query($conn, $sql);
     echo "Data inserted successfully!";
@@ -22,7 +24,7 @@ if(mysqli_num_rows($result) == 0) {
 else {
     $row = mysqli_fetch_assoc($result);
     if($score > $row['score']) {
-        $sql = "UPDATE Players SET score='$score', mode='$mode' WHERE username='$username'";
+        $sql = "UPDATE Players SET score='$score', mode='$mode', timestamp='$timestamp' WHERE username='$username'";
         mysqli_query($conn, $sql);
         echo "New high score achieved!";
     }
